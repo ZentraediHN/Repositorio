8b462d26abcb910a656b069416b22f76bf3e4f61
Jorge Laínez <jlainezbonilla@gmail.com>
2024-10-19 22:37:11 -0600

    Actualización del script CrearDirFecha.sh

--- CrearDirFecha.sh
+++ CrearDirFecha.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
-#Script para crear directorios o carpetas segun las fechas [año y mes] de los archivos que existan y moverlos a la vez
-# a cada carpeta creada.
 
-SAVEIFS=$IFS								#establecer el delimitador 
-IFS=$(echo -en "\n\b")						#establecer el delimitador que sea un retorno o un 
+#=========================================================================
+##  Script to create directories or folders based on the date [year and month]
+##  of existing files and simultaneously move them to each created folder.
+##
+##  This script is licensed under the GNU General Public License (GPL).
+##  You can copy, distribute and modify this software under the terms of the GPL.
+##  For more information, see: https://www.gnu.org/licenses/gpl-3.0.en.html
+#=========================================================================
 
-for FILE in $(ls);							#se busca en el directorio recursivamente cada archivo en terminacion "txt" o el que se especifique
+#  Save the original IFS
+SAVEIFS=$IFS
+# Set the internal field separator to newline or backspace
+IFS=$(echo -en "\n\b")
+
+
+#--------Iterate over each file in the current directory--------
+for FILE in $(ls);
 do
-	if [ -f $FILE ];
+#--------Checks if it is a file and not a directory or link--------
+	if [ -f "$FILE" ];
 		then
-		NEWFILE=`stat -c%y $FILE | cut -c1-7`; #A cada archivo encontrado se tomara su fecha mes y año
-		if [ -d $NEWFILE ];						# Se comprueba si existe el directorio con el nuevo nombre creado "Newfile"
+#--------For each file found, its month and year date will be taken--------
+		NEW_DIRECTORY=`stat -c%y "$FILE" | cut -c1-7`;
+#--------Checks if the directory with the new created name "NewDirectory" exists--------
+		if [ -d "$NEW_DIRECTORY" ];
 			then
-			mv $FILE $NEWFILE					# De existir el directorio "Newfile" solo se procede a mover el archivo
+#--------If the directory "NewDirectory" exists, only the file is moved--------
+			mv "$FILE" "$NEW_DIRECTORY"
 		else
-			mkdir "$NEWFILE";					#Sino se crea cada carpeta con el nuevo nombre
-			mv $FILE $NEWFILE					#Y se procede a mover cada archivo original a cada carpeta creada respectivamente.
+#--------If not, each folder is created with the new name--------
+			mkdir "$NEW_DIRECTORY";
+#--------And each original file is moved to each created folder respectively.--------
+			mv "$FILE" "$NEW_DIRECTORY"
 		fi
 	else
 		echo -e "$FILE no es un regular file"
 	fi
 done
+#	Restore the delimiter
 IFS=$SAVEIFS
