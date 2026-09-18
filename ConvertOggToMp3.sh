39f0b6fa145d8837f92824fdbe5640bfbf89e2d0
Jorge Laínez <jlainezbonilla@gmail.com>
2026-09-18 13:40:47 -0600

    Update ConvertOggToMp3.sh

--- ConvertOggToMp3.sh
+++ ConvertOggToMp3.sh
@@ -3,7 +3,7 @@
 # Script para convertir archivos de audio .ogg a mp3
 #---------------------------------------------------
 
-for i in *ogg;
+for i in *.ogg;
 do
 ogg123 -d wav -f - "$i" | lame - "${i/ogg/mp3}";
 done
